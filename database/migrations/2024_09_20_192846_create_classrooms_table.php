bb116bc4ff5f96b51abbf81396d5643570b44fb3
in-th3-l00p <indianucode0303@gmail.com>
2024-09-20 23:34:08 +0300

    fixed constrain typo

--- database/migrations/2024_09_20_192846_create_classrooms_table.php
+++ database/migrations/2024_09_20_192846_create_classrooms_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->text("description");
             $table
                 ->foreignId("owner_id")
-                ->constrained("user")
+                ->constrained("users")
                 ->onDelete("cascade");
         });
     }
