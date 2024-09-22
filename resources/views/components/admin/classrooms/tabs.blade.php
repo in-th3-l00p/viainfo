27ff8b198c55c8c0cd6ac97113ea6768ec212f2a
in-th3-l00p <indianucode0303@gmail.com>
2024-09-22 15:40:02 +0300

    people rounded fixed

--- resources/views/components/admin/classrooms/tabs.blade.php
+++ resources/views/components/admin/classrooms/tabs.blade.php
@@ -30,7 +30,7 @@
                 <button
                     type="button"
                     class="group relative min-w-0 flex-1 overflow-hidden bg-white px-4 py-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
-                    x-bind:class="tab === 'people' ? 'rounded-l-md text-gray-900' : 'text-gray-500 hover:text-gray-700'"
+                    x-bind:class="tab === 'people' ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700'"
                     x-bind:aria-current="tab === 'people' ? 'page' : null"
                     @click="tab = 'people'"
                 >
