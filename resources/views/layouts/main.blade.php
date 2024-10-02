a5116ee8180dc1798512848c0e8a1e4bf76e72d4
in-th3-l00p <indianucode0303@gmail.com>
2024-10-02 15:20:19 +0300

    favicon & title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WSRO Web</title>
+    <title>Hai La Olimpiada</title>
 
     @vite([
         "resources/scss/app.scss",
