22f253f6b5f5aa2a3a3b907fd831d555b86d4690
in-th3-l00p <indianucode0303@gmail.com>
2024-10-10 18:36:24 +0300

    landing components organization

---
+++ resources/views/components/landing/about.blade.php
@@ -0,0 +1,16 @@
+<div class="overflow-hidden bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <div class="lg:pr-8 lg:pt-4">
+                <div class="lg:max-w-lg">
+                    <h2 class="text-base font-semibold leading-7 text-indigo-600">Despre Hai la Olimpiadă!</h2>
+                    <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Un program dedicat pasiunii și performanței</p>
+                    <p class="mt-6 text-lg leading-8 text-gray-600">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consequatur delectus dolorum eaque enim error pariatur perferendis placeat porro quam quis, ratione repellendus sapiente sed sunt suscipit tenetur ullam veritatis.
+                    </p>
+                </div>
+            </div>
+            <img src="/static/home/about.jpg" alt="Pregătire Olimpiadă" class="w-full max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[48rem] md:-ml-4 lg:-ml-0" width="2432" height="1442">
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/landing/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="bg-gray-900" aria-labelledby="footer-heading">
+    <h2 id="footer-heading" class="sr-only">Footer</h2>
+    <div class="mx-auto px-4 py-6">
+        <div class="flex gap-4 justify-center items-center">
+            <img
+                class="h-12 bg-white rounded-full p-1 border"
+                src="/static/logo.svg"
+                alt="logo"
+            >
+            <p class="text-white">Toate drepturile sunt rezervate. ©</p>
+        </div>
+    </div>
+</footer>
