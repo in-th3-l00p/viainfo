5d6248f9a44f0b96e5fc03f3ea41b16216bf058d
in-th3-l00p <indianucode0303@gmail.com>
2024-10-10 16:50:09 +0300

    landing page basics

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,11 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
 class IndexController extends Controller {
-    public function __invoke(Request $request) {
-        if (!$request->user())
-            return redirect()->route("login");
-        if ($request->user()->role === "admin")
-            return redirect()->route("admin.dashboard");
-        return redirect()->route("user.dashboard");
+    public function __invoke() {
+        return view("landing");
     }
 }
