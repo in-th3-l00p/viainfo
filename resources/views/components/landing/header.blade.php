b6dc69bf581f3e1bd6ca4cdd145fea8cba2a68dd
in-th3-l00p <indianucode0303@gmail.com>
2024-10-18 10:03:17 +0300

    fixed landing page sidebar

--- resources/views/components/landing/header.blade.php
+++ resources/views/components/landing/header.blade.php
@@ -83,7 +83,15 @@
                         <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ __("Contact") }}</a>
                     </div>
                     <div class="py-6">
-                        <a href="{{ route("login") }}" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ __("Loghează-te") }}</a>
+                        @guest
+                            <a href="{{ route("login") }}" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ __("Loghează-te") }}</a>
+                        @endguest
+                        @auth
+                            <a
+                                href="{{ route(request()->user()->role . ".dashboard") }}"
+                                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                            >{{ __("Dashboard") }}</a>
+                        @endauth
                     </div>
                 </div>
             </div>
