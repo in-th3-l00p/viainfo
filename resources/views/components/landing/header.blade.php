4c043b0c76c1be9cce52d0c26e523d1b739f33ef
in-th3-l00p <indianucode0303@gmail.com>
2024-10-18 10:04:13 +0300

    fixed landing page sidebar x2

--- resources/views/components/landing/header.blade.php
+++ resources/views/components/landing/header.blade.php
@@ -79,8 +79,8 @@
             <div class="mt-6 flow-root">
                 <div class="-my-6 divide-y divide-gray-500/10">
                     <div class="space-y-2 py-6">
-                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ __("Despre") }}</a>
-                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ __("Contact") }}</a>
+                        <a href="#despre" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ __("Despre") }}</a>
+                        <a href="#contact" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ __("Contact") }}</a>
                     </div>
                     <div class="py-6">
                         @guest
